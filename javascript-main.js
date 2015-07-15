a68ccc30c2b537ca296703a7f99d4aa7fbae3305
bartas <bartekkubala@wp.pl>
2015-07-15 13:35:28 +0200

    Fix Js problem

--- javascript-main.js
+++ javascript-main.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     console.log('dupa');
-
-    function myFunction() {
-        var x = document.getElementById("logo1")
-        x.innerHTML = Math.floor((Math.random() * 100) + 1);
-    }
 });
+
+function myFunction() {
+    var x = document.getElementById("logo1")
+    x.innerHTML = Math.floor((Math.random() * 100) + 1);
+}
